ae28f6151a224d74e3e8f5048dba82b4a2ecd634
Talal Saad <talalsadd@gmail.com>
2023-05-08 15:17:07 +0300

    updated image text extracter

--- modle/imageTextExtracter.py
+++ modle/imageTextExtracter.py
@@ -16,46 +16,85 @@ def get_drive_file_id(link):
         return None
 
     
+def extract_ram(data):
+    match = re.search(r'Installed\s+RAM\s+(.*?)\s+', data)
+    if match:
+        return match.group(1)
+    match = re.search(r'Installed\s+memory\s+\(RAM\)\s+(.*?)\s+', data)
+    if match:
+        return match.group(1)
+    return None
 
+
+def extract_device_name(data):
+    match = re.search(r'Device name ?\s+(.*)', data)
+    if match:
+        return match.group(1)
+    
+    match = re.search(r'Computer name ?\s+(.*)', data)
+    if match:
+        return match.group(1)
+    return None
+   
+"""
+    'https://drive.google.com/file/d/1xxbtmSSrxO0-ZIpSgYRPAr-m1L9MQ2Lu/view?usp=share_link',
+    'https://drive.google.com/file/d/1YERKgFTc7Ru8UJUrWk19Fm1VUhSaPgz4/view?usp=share_link',
+    'https://drive.google.com/file/d/1J72Ep1ZW47HSf0s1yVyFYDzntwR2eHld/view?usp=share_link',
+    'https://drive.google.com/file/d/1VxzudZpD6av5GBFJc7cm2KLdw7CfgEgW/view?usp=share_link'
+
+"""
+    
+#'55 - Copy.png'
 
 urls = [
     
-    'https://drive.google.com/file/d/1aGXDN6ILD1qKPggpD80QHjIPuytpfDqV/view?usp=share_link',
-    'https://drive.google.com/file/d/1RDTkOnkfSSD3qTAgkIGOMUpbz0BLrX4X/view?usp=share_link'
-    
+   
+    'https://drive.google.com/file/d/1xxbtmSSrxO0-ZIpSgYRPAr-m1L9MQ2Lu/view?usp=share_link',
+    'https://drive.google.com/file/d/1Ex0cVMl_I5P2IX9Cj-nlG70p-jl6x5Iy/view?usp=share_link',
+    'https://drive.google.com/file/d/1VxzudZpD6av5GBFJc7cm2KLdw7CfgEgW/view?usp=share_link'
+
     
 ]
 for url in urls:
     url=f'https://drive.google.com/uc?export=download&id={get_drive_file_id(url)}'
     response = requests.get(url)
     
-    #image = Image.open(io.BytesIO(response.content))
+    ##image = Image.open(url)
 
-    with open('.\\images\\test.png', 'wb') as f:
+    with open('.\\images\\test', 'wb') as f:
         f.write(response.content)
+
+        
     time.sleep(2)
-    image = Image.open('.\\images\\test.png')
+    #image = Image.open(f'.\\images\\{url}')
+    image = Image.open('.\\images\\test')
     
 
     # Perform text extraction
-    data = pytesseract.image_to_string(image, lang='eng', config='--psm 6')
-    print(data)
+    data = pytesseract.image_to_string(image,lang = 'eng',config='--psm 6')
+    
     fields = {
     'Device name': '',
     'Processor': '',
-    'Installed': '',
+    'RAM': '',
     'System type': '',
 }
 
-for field in fields:
-    pattern = field + r':?\s+(.*)'
-    match = re.search(pattern, data)
-    if match:
-        fields[field] = match.group(1)
-        # Get the entire line of text containing the match
-        match_line = re.search(r'.*{}.*'.format(match.group(1)), data)
-        if match_line:
-            fields[field] = match_line.group(0)
-
     
+    if(extract_device_name(data)!=None):
+        fields['Device name'] =extract_device_name(data)
+
+    match = re.search(r'Processor:?\s+.*(Intel.*)', data)
+    if match:
+        fields['Processor'] = match.group(1)
+
+
+    if(extract_ram(data)!=None):
+        fields['RAM'] = extract_ram(data)
+    
+
+    match = re.search(r'System type?\s+(.*)', data)
+    if match:
+        fields['System type'] = match.group(1)
+        
     print(fields)
